78683d16935710622de27bbe9fa2cd2bca4919d8
Kimberly Robasky <krobasky@cellfie2.renci.org>
2019-08-08 07:33:38 -0400

    added the swagger API

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.api_response import ApiResponse
+from swagger_server.models.global_options import GlobalOptions
+from swagger_server.models.job import Job
+from swagger_server.models.local_options import LocalOptions
