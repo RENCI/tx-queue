5c8dff8b7b427a2f70a8e928659c5bc3d6d26a74
Kimberly Robasky <krobasky@cellfie2.renci.org>
2019-08-09 03:16:07 -0400

    hooking up both api versions to the config/server/params.py file

--- api/swagger2/swagger_server/__main__.py
+++ api/swagger2/swagger_server/__main__.py
@@ -2,24 +2,34 @@
 
 import sys
 sys.path.append('../..')
-import config.server.params as params
+hasconfig=True
+try:
+  import config.server.params as params
+except ImportError:
+  hasconfig=False
 
 import connexion
 
 from swagger_server import encoder
 
 def main():
+    port=8080 # default
+
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    if params.basePath != "":
-        app.add_api('swagger.yaml', base_path=params.basePath, arguments={'title': 'RENCI Translational Sciece simple queue API: Swagger v2'})
-    else:
-        app.add_api('swagger.yaml', arguments={'title': 'RENCI Translational Sciece simple queue API: Swagger v2'})
-    if params.port != "":
-        app.run(port=params.port)
-    else:
-        app.run()
 
+    addApi=True
+    if hasconfig:
+      if params.port != "":
+        port=params.port
+      if params.basePath != "":
+        app.add_api('swagger.yaml', base_path=params.basePath, arguments={'title': 'RENCI Translational Sciece simple queue API: Swagger v2'})
+        addApi=False
+
+    if addApi:
+      app.add_api('swagger.yaml', arguments={'title': 'RENCI Translational Sciece simple queue API: Swagger v2'})
+
+    app.run(port=port)
 
 if __name__ == '__main__':
     main()
